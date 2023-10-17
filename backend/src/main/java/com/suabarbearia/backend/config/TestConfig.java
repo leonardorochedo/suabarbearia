03930439a074f3108001e32ed437cc1372158a44
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-10-17 15:46:56 -0300

    feat: User repository, TestConfig file

---
+++ backend/src/main/java/com/suabarbearia/backend/config/TestConfig.java
@@ -0,0 +1,28 @@
+package com.suabarbearia.backend.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.suabarbearia.backend.entities.User;
+import com.suabarbearia.backend.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		User u1 = new User(null, "Leonardo Rochedo", "leonardo@email.com", "123321", "33981111", null);
+		User u2 = new User(null, "Gui Molari", "gui@email.com", "123321", "33982222", null);
+
+		userRepository.saveAll(Arrays.asList(u1, u2)); // Save a list of users in testdb when initialize app
+	}
+
+}
