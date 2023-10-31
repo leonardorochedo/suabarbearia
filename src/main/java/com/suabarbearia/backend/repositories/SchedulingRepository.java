e593bc36b619391df2149d7648a4c4a38f023039
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-10-31 08:02:30 -0300

    change structure

--- src/main/java/com/suabarbearia/backend/repositories/SchedulingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.suabarbearia.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.suabarbearia.backend.entities.Scheduling;
-
-public interface SchedulingRepository extends JpaRepository<Scheduling, Long> {
-}
--- src/main/java/com/suabarbearia/backend/repositories/ServiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.suabarbearia.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.suabarbearia.backend.entities.Service;
-
-public interface ServiceRepository extends JpaRepository<Service, Long> {
-}
