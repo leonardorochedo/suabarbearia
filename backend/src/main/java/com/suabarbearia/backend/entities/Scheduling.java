77d3c86598f6a2f960fb6b9750d5c0af9befe6d2
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2024-01-23 09:15:11 -0300

    fix: ENUM in Scheduling

--- backend/src/main/java/com/suabarbearia/backend/entities/Scheduling.java
+++ backend/src/main/java/com/suabarbearia/backend/entities/Scheduling.java
@@ -5,13 +5,7 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.suabarbearia.backend.enums.Status;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_scheduling")
@@ -51,6 +45,7 @@ public class Scheduling implements Serializable {
 
 	private LocalDateTime date;
 
+	@Enumerated(EnumType.STRING)
 	private Status status;
 
 	// Efi
